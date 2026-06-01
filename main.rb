b81fc3760c7b9ac9555bc2a1a8afa0673514cb14
markus <markusaurelius04@gmail.com>
2026-06-01 10:43:47 -0300

    atualizando a main

--- main.rb
+++ main.rb
@@ -1,24 +1,32 @@
 require_relative "lib/util"
+require_relative "views/pessoa_view"
+require_relative "data/pessoa_data"
 
 tela = Util.new
+pessoa_data = PessoaData.new
+pessoa_view = PessoaView.new(pessoa_data)
+
 loop = true
 
 while loop
   
-  tela.gerarTitulo "Sistema Bancario"
+  tela.gerar_titulo "Sistema Bancario"
   print "Escolha: 1 - Pessoa | 2 - Conta  "
   escolha = gets.chomp.to_i
 
   case escolha
-    when 1 
-      p "Tela de Pessoas"
+    when 1
+      tela.gerar_titulo "SB - Pessoa"
+      pessoa_view.menu_pessoa  
     when 2 
+      tela.gerar_titulo "SB - Contas"
       p "Tela de contas"
   else
-    p "Operação inválida"
+    p "Operação Inválida!"
   end
 
   print "Deseja continuar? S - sim | N - não  "
   continuar = gets.chomp
   loop = false if continuar.upcase == "N"
+
 end
