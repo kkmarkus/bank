2bb35403d6b8bb694c66e8153e973086503c6f9d
markus <markusaurelius04@gmail.com>
2026-06-02 07:58:16 -0300

    correção na visualização

--- views/pessoa_view.rb
+++ views/pessoa_view.rb
@@ -16,10 +16,16 @@ class PessoaView
 
       case escolha
         when 1
-          p "   ID    |   CPF/CNPJ    |   NOME    |   TIPO   |    CIDADE/UF   "
-          @data.listar.each_with_index do |p, index|
-            p "#{index + 1} | #{p.cpf_cnpj} | #{p.nome} | #{p.tipo} | #{p.endereco.cidade}/#{p.endereco.uf} "
+          largura = 75
+          puts ""
+          puts "-" * largura
+          puts " #{"ID".center(4)} | #{"CPF/CNPJ".center(14)} | #{"NOME".center(30)} | #{"TIPO".center(4)} | CIDADE/UF"
+          puts "-" *largura
+          @data.listar.each_with_index do |pessoa, index|
+            cidade_uf = "#{pessoa.endereco.cidade}/#{pessoa.endereco.uf}"
+            puts " #{(index + 1).to_s.center(4)} | #{pessoa.cpf_cnpj.ljust(14)} | #{pessoa.nome.ljust(30)} | #{pessoa.tipo.center(4)} | #{cidade_uf}"
           end
+          puts ""
         when 2
           print "Informe o CPF/CNPJ: "
           registro = gets.chomp
